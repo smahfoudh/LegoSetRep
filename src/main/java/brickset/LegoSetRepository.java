ec33cea982afa2ba99cfa870d6f448c871f9e2fe
Skander Mahfoudh <skan.mh@gmail.com>
2022-04-15 00:04:40 +0200

    pushing the project

---
+++ src/main/java/brickset/LegoSetRepository.java
@@ -0,0 +1,15 @@
+package brickset;
+
+import repository.Repository;
+
+/**
+ * Represents a repository of {@code LegoSet} objects.
+ */
+public class LegoSetRepository extends Repository<LegoSet> {
+
+    public LegoSetRepository() {
+        super(LegoSet.class, "brickset.json");
+    }
+
+
+}
