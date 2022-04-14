ea27622f5fe57efbaa5e3a3bc946579387a9c3a1
smahfoudh <skan.mh@gmail.com>
2022-04-15 00:10:48 +0200

    Added first .anyMatch method

--- src/main/java/brickset/LegoSetRepository.java
+++ src/main/java/brickset/LegoSetRepository.java
@@ -11,5 +11,23 @@ public class LegoSetRepository extends Repository<LegoSet> {
         super(LegoSet.class, "brickset.json");
     }
 
+    /**
+     * Returns whether  there is match of LEGO sets (at least one) with the specified theme
+     *
+     * @param themeName a LEGO set theme
+     * @return True or False (Whether there are LEGO sets with the specified theme or not)
+     */
+    public boolean SeeLegoSetsWithTheme(String themeName) {
 
+        return getAll().stream()
+                .anyMatch(LegoSet ->LegoSet.getTheme().contains(themeName));
+    }
+
+
+    public static void main(String[] args) {
+        var repository = new LegoSetRepository();
+        System.out.println(repository.SeeLegoSetsWithTheme("Star Wars"));
+
+
+    }
 }
